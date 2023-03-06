f2e7b8929f3c414880be5d6e9e6fd4d78e86ceb2
John <john.chang@starnic.com.tw>
2023-03-06 20:29:37 +0800

    use nvme1n1

--- install.sh
+++ install.sh
@@ -9,12 +9,15 @@ DRIVER_BASE_URL=https://us.download.nvidia.com/tesla
 DRIVER_VERSION=525.85.12
 
 # 內建的nvme1
-sudo file -s /dev/nvme1n1
-sudo mkfs -t xfs /dev/nvme1n1
-sudo mkdir /data
-sudo mount /dev/nvme1n1 /data
-sudo chmod 777 /data
-echo '/dev/nvme1n1 /data ext4 discard,defaults,nofail 0 2' | sudo tee -a /etc/fstab
+if [ ! -d "/data" ]
+then
+    sudo file -s /dev/nvme1n1
+    sudo mkfs -t xfs /dev/nvme1n1
+    sudo mkdir /data
+    sudo mount /dev/nvme1n1 /data
+    sudo chown ${USER}.${USER} /data
+    echo '/dev/nvme1n1 /data ext4 discard,defaults,nofail 0 2' | sudo tee -a /etc/fstab
+fi
 
 # 安裝系統套件
 sudo apt update
@@ -24,9 +27,14 @@ sudo apt install -y wget curl aria2 \
 
 # 安裝顯示卡驅動
 # https://www.nvidia.com/Download/Find.aspx
-cd /tmp
-curl -fSsl -O $DRIVER_BASE_URL/$DRIVER_VERSION/NVIDIA-Linux-$ARCH-$DRIVER_VERSION.run
-sudo sh NVIDIA-Linux-$ARCH-$DRIVER_VERSION.run -s
+if [ ! -f "/root/.nvdirve_installed" ]
+then
+    cd /tmp
+    curl -fSsl -O $DRIVER_BASE_URL/$DRIVER_VERSION/NVIDIA-Linux-$ARCH-$DRIVER_VERSION.run
+    sudo sh NVIDIA-Linux-$ARCH-$DRIVER_VERSION.run -s
+    rm NVIDIA-Linux-$ARCH-$DRIVER_VERSION.run
+    sudo touch /root/.nvdirve_installed
+fi
 
 # 安裝stable-diffusion-webui
 cd ${HOME_DIR}
--- run-instances.sh
+++ run-instances.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
-AWS_CLI="docker run --rm -ti -v ~/.aws:/root/.aws amazon/aws-cli"
-
-${AWS_CLI} ec2 run-instances \
+docker run --rm -ti -v ~/.aws:/root/.aws amazon/aws-cli \
+ec2 run-instances \
     --count 1 \
     --instance-type g4dn.xlarge \
     --image-id ami-0557a15b87f6559cf \
     --key-name stable-diffusion-webui \
     --security-group-ids sg-0f90be5d16639339e \
     --instance-market-options '{"MarketType":"spot","SpotOptions":{"InstanceInterruptionBehavior":"terminate","MaxPrice":"0.2","SpotInstanceType":"one-time"}}' \
-    --block-device-mappings '{"DeviceName":"/dev/sda1","Ebs":{"VolumeSize":100,"VolumeType":"gp3"}}' \
+    --block-device-mappings '{"DeviceName":"/dev/sda1","Ebs":{"VolumeSize":10,"VolumeType":"gp3"}}' \
     --region us-east-1 \
     --user-data '#! /bin/bash
         bash <(wget -qO- https://raw.githubusercontent.com/GPXCAT/sd-ec2-install/main/install.sh)
     ' \
     --output table
-
-# ${AWS_CLI} ec2 run-instances \
-# ec2 describe-instances \
-#     --region us-east-1
